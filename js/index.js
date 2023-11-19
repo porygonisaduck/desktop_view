e56e67b873bcdfc71b71bb3e4039e4268e9be22c
porygonisaduck <alvaradx@umich.edu>
2023-11-19 13:02:01 -0500

    Completed more than half requirements

---
+++ js/index.js
@@ -0,0 +1,24 @@
+let pets = document.querySelectorAll(".pet");
+
+for (let i = 0; i < pets.length; ++i) {
+    let pet =  pets[i];
+    let thumbnails = pet.querySelectorAll(".thumbnail");
+
+    for (let i = 0; i < thumbnails.length; ++i){
+        thumbnails[i].addEventListener("mouseover", function() {
+            let display = pet.querySelector("#display");
+            display.alt = this.alt;
+            display.setAttribute('og', display.src);
+            display.src = this.src;
+        });
+
+        thumbnails[i].setAttribute("tabindex", 0);
+
+        thumbnails[i].addEventListener("mouseleave", function() {
+            let display = pet.querySelector("#display");
+            display.src = display.getAttribute("og");
+            display.innerHTML = "Hover over an image below to display the image and the alt text.";
+
+        });
+    }
+}
