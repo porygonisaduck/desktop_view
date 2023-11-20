4a85d7ffefc7dc46959dc0c3626f70726d00f3bf
porygonisaduck <alvaradx@umich.edu>
2023-11-20 12:50:34 -0500

    Fixed a couple of decorative issues and button issue

--- js/button.js
+++ js/button.js
@@ -46,7 +46,7 @@ function myHide(name) {
     }
 
     for (let i = 0; i < hide.length; i+=defaults) {
-        if (hide[i].getAttribute('name') == name) {
+        if (hide[i].getAttribute('id') == name) {
             hide[i].style.display = "none";
             try {
                 hide[i+1].style.display = "none";
